d21b03dcac0ddbf320058eadf0be233f9e25ae5f
HanBeom <fode6733@gmail.com>
2020-07-19 20:14:46 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  memo
+//
+//  Created by CHOIHANBEOM on 2020/07/19.
+//  Copyright © 2020 CHOIHANBEOM. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
